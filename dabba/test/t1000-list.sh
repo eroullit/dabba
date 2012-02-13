81b9b6c431a27399340138982a30d5359c6ea4b2
Emmanuel Roullit <emmanuel.roullit@gmail.com>
2012-02-13 22:50:59 +0100

    Start properly dabbad daemon before executing test.

--- dabba/test/t1000-list.sh
+++ dabba/test/t1000-list.sh
@@ -7,8 +7,9 @@ DABBA_PATH="$TEST_DIRECTORY/../../dabba"
 
 test_expect_failure 'invoke dabba list w/o dabbad' "$DABBA_PATH/dabba list"
 test_expect_success 'invoke dabba list with dabbad' "
-    $DABBAD_PATH/dabbad & &&
-    $DABBA_PATH/dabba list
+    $DABBAD_PATH/dabbad --daemonize &&
+    $DABBA_PATH/dabba list &&
+    killall dabbad
 "
 
 test_done
