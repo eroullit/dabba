ce217621233edd30e71d23da988ceacf29787f1a
Emmanuel Roullit <emmanuel.roullit@gmail.com>
2012-02-14 17:38:12 +0100

    Fetch the expected interfaces from /proc/net/dev.
    This enables the unit test to fetch the interface names via /proc/net/dev.
    The same interfaces are awaited in 'dabba list' output.

--- dabba/test/t1000-list.sh
+++ dabba/test/t1000-list.sh
@@ -5,11 +5,31 @@ test_description='Test dabba list command'
 DABBAD_PATH="$TEST_DIRECTORY/../../dabbad"
 DABBA_PATH="$TEST_DIRECTORY/../../dabba"
 
+generate_list(){
+for dev in `sed '1,2d' /proc/net/dev | awk -F ':' '{ print $1 }' | tr -d ' '`
+do
+    echo "    - $dev" >> dev_list
+done
+}
+
+generate_yaml_list()
+{
+generate_list
+
+cat <<EOF
+---
+  interfaces:
+`cat dev_list`
+EOF
+}
+
 test_expect_failure 'invoke dabba list w/o dabbad' "$DABBA_PATH/dabba list"
 test_expect_success 'invoke dabba list with dabbad' "
     $DABBAD_PATH/dabbad --daemonize &&
-    $DABBA_PATH/dabba list &&
-    killall dabbad
+    $DABBA_PATH/dabba list > result &&
+    killall dabbad &&
+    generate_yaml_list > expected &&
+    test_cmp expected result
 "
 
 test_done
