e8279dfb3058b0802b86a09885469086b5c31739
Emmanuel Roullit <emmanuel.roullit@gmail.com>
2012-04-11 08:01:10 +0200

    Added capture unit test stubs.

---
+++ dabba/test/t1100-capture.sh
@@ -0,0 +1,51 @@
+#!/bin/sh
+#
+# Copyright (C) 2009-2012	Emmanuel Roullit <emmanuel.roullit@gmail.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or (at
+# your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+# or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+# for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin St, Fifth Floor, Boston, MA 02110, USA
+#
+
+test_description='Test dabba capture command'
+ring_size=$((16*1024*1024))
+
+. ./dabba-test-lib.sh
+
+test_expect_success "Setup: Start dabbad" "
+    $DABBAD_PATH/dabbad --daemonize
+"
+
+test_expect_success "Start a capture thread on loopback" "
+    $DABBA_PATH/dabba capture start --device lo --pcap test1.pcap --size $ring_size
+"
+
+test_expect_success "List created capture thread" "
+    $DABBA_PATH/dabba capture list > result
+"
+
+test_expect_success "Start a second capture thread on loopback" "
+    $DABBA_PATH/dabba capture start --device lo --pcap test2.pcap --size $ring_size
+"
+
+test_expect_success "List created capture thread" "
+    $DABBA_PATH/dabba capture list > result
+"
+
+test_expect_success "Cleanup: Stop dabbad" "
+    killall dabbad
+"
+
+test_done
+
+# vim: ft=sh:tabstop=4:et
