b1829dc26189c49f9a1f6902b686d7e2a3127a0f
Emmanuel Roullit <emmanuel.roullit@gmail.com>
2012-09-09 22:05:35 +0200

    test: use dict python scripts in 'dabba capture'.
    Signed-off-by: Emmanuel Roullit <emmanuel.roullit@gmail.com>

--- dabba/test/t1100-capture.sh
+++ dabba/test/t1100-capture.sh
@@ -24,14 +24,14 @@ test_description='Test dabba capture command'
 get_capture_thread_nr()
 {
     local result_file=$1
-    python -c "import yaml; y = yaml.load(open('$result_file')); print len(y['captures']);"
+    "$PYTHON_PATH" -c "import yaml; y = yaml.load(open('$result_file')); print len(y['captures']);"
 }
 
 get_capture_thread_id()
 {
     local thread_nr=$1
     local result_file=$2
-    python -c "import yaml; y = yaml.load(open('$result_file')); print y['captures'][$thread_nr]['id'];"
+    "$PYTHON_PATH" -c "import yaml; y = yaml.load(open('$result_file')); print y['captures'][$thread_nr]['id'];"
 }
 
 check_capture_thread_nr()
@@ -40,50 +40,6 @@ check_capture_thread_nr()
     local result_file=$2
     local result_thread_nr="$(get_capture_thread_nr $result_file)"
     test "$expected_thread_nr" = "$result_thread_nr"
-}
-
-check_capture_thread_id()
-{
-    local thread_nr=$1
-    local result_file=$2
-    local result_thread_id="$(get_capture_thread_id $thread_nr $result_file)"
-    echo "$result_thread_id" | grep -w -q -E "^[0-9]+"
-}
-
-check_capture_thread_interface()
-{
-    local thread_nr=$1
-    local expected_interface=$2
-    local result_file=$3
-    local result_interface="$(python -c "import yaml; y = yaml.load(open('$result_file')); print y['captures'][$thread_nr]['interface'];")"
-    test "$expected_interface" = "$result_interface"
-}
-
-check_capture_thread_pcap()
-{
-    local thread_nr=$1
-    local expected_pcap="$2"
-    local result_file=$3
-    local result_pcap="$(python -c "import yaml; y = yaml.load(open('$result_file')); print y['captures'][$thread_nr]['pcap'];")"
-    test "$expected_pcap" = "$result_pcap"
-}
-
-check_capture_thread_packet_mmap_size()
-{
-    local thread_nr=$1
-    local expected_packet_mmap_size=$2
-    local result_file=$3
-    local result_packet_mmap_size="$(python -c "import yaml; y = yaml.load(open('$result_file')); print y['captures'][$thread_nr]['packet mmap size'];")"
-    test "$expected_packet_mmap_size" = "$result_packet_mmap_size"
-}
-
-check_capture_thread_frame_nr()
-{
-    local thread_nr=$1
-    local expected_frame_nr=$2
-    local result_file=$3
-    local result_frame_nr="$(python -c "import yaml; y = yaml.load(open('$result_file')); print y['captures'][$thread_nr]['frame number'];")"
-    test "$expected_frame_nr" = "$result_frame_nr"
 }
 
 default_frame_nr="32"
@@ -128,21 +84,27 @@ test_expect_success "Start capture thread with a default frame number" "
     '$DABBA_PATH'/dabba capture list > result
 "
 
+test_expect_success PYTHON_YAML "Parse capture YAML output" "
+    yaml2dict result > parsed
+"
+
 test_expect_success PYTHON_YAML "Check thread default capture frame number ($default_frame_nr)" "
-    check_capture_thread_frame_nr 0 $default_frame_nr result
+    yaml2dict result > parsed
+    test \"$(dictkeys2values captures 0 'frame number' < parsed)\" = '$default_frame_nr'
 "
 
 test_expect_success PYTHON_YAML "Stop capture thread with a default frame number" "
-    '$DABBA_PATH'/dabba capture stop --id `get_capture_thread_id 0 result` &&
+    '$DABBA_PATH'/dabba capture stop --id \"$(dictkeys2values captures 0 id < parsed)\" &&
     '$DABBA_PATH'/dabba capture list > after &&
-    test_must_fail grep -wq `get_capture_thread_id 0 result` after
+    test_must_fail grep -wq \"$(dictkeys2values captures 0 id < parsed)\" after
 "
 
 for i in `seq 0 9`
 do
         test_expect_success "Start capture thread #$(($i+1)) on loopback" "
             '$DABBA_PATH'/dabba capture start --interface any --pcap test$i.pcap --frame-number $frame_nr &&
-            '$DABBA_PATH'/dabba capture list > result
+            '$DABBA_PATH'/dabba capture list > result &&
+            yaml2dict result > parsed
         "
 
         test_expect_success PYTHON_YAML "Check thread #$(($i+1)) number" "
@@ -150,32 +112,32 @@ do
         "
 
         test_expect_success PYTHON_YAML "Check thread #$(($i+1)) ID" "
-            check_capture_thread_id $i result
+            echo \"$(dictkeys2values captures $i id < parsed)\" | grep -wq -E '^[0-9]+'
         "
 
         test_expect_success PYTHON_YAML "Check thread #$(($i+1)) capture interface" "
-            check_capture_thread_interface $i any result
+            test \"$(dictkeys2values captures $i interface < parsed)\" = 'any'
         "
 
         test_expect_success PYTHON_YAML "Check thread #$(($i+1)) capture pcap file" "
-            check_capture_thread_pcap $i \"$PWD/test$i.pcap\" result
+            test \"$(dictkeys2values captures $i pcap < parsed)\" = '$(pwd)/test$i.pcap'
         "
 
         test_expect_success PYTHON_YAML "Check thread #$(($i+1)) capture packet mmap size" "
-            check_capture_thread_packet_mmap_size $i $ring_size result
+            test \"$(dictkeys2values captures $i 'packet mmap size' < parsed)\" = '$ring_size'
         "
 
         test_expect_success PYTHON_YAML "Check thread #$(($i+1)) capture frame number" "
-            check_capture_thread_frame_nr $i $frame_nr result
+            test \"$(dictkeys2values captures $i 'frame number' < parsed)\" = '$frame_nr'
         "
 done
 
 for i in `seq 0 9`
 do
         test_expect_success PYTHON_YAML "Stop capture thread #$(($i+1)) on loopback" "
-            '$DABBA_PATH'/dabba capture stop --id `get_capture_thread_id $i result` &&
+            '$DABBA_PATH'/dabba capture stop --id \"$(dictkeys2values captures $i id < parsed)\" &&
             '$DABBA_PATH'/dabba capture list > after &&
-            test_must_fail grep -wq `get_capture_thread_id $i result` after
+            test_must_fail grep -wq \"$(dictkeys2values captures $i id < parsed)\" after
         "
 done
 
