5dfe2de3d99e5f787c3396f32e63d1aeae972672
Emmanuel Roullit <emmanuel.roullit@gmail.com>
2012-11-06 19:17:39 +0100

    dabbad: add rpc function to fetch pause data.
    Signed-off-by: Emmanuel Roullit <emmanuel.roullit@gmail.com>

--- dabbad/include/dabbad/interface-pause.h
+++ dabbad/include/dabbad/interface-pause.h
@@ -1,9 +1,3 @@
-/**
- * \file rpc.c
- * \author written by Emmanuel Roullit emmanuel.roullit@gmail.com (c) 2012
- * \date 2012
- */
-
 /* __LICENSE_HEADER_BEGIN__ */
 
 /*
@@ -27,31 +21,14 @@
 
 /* __LICENSE_HEADER_END__ */
 
-#include <google/protobuf-c/protobuf-c-rpc.h>
+#ifndef INTERFACE_PAUSE_H
+#define	INTERFACE_PAUSE_H
+
 #include <libdabba-rpc/dabba.pb-c.h>
-#include <dabbad/interface.h>
-#include <dabbad/interface-list.h>
-#include <dabbad/interface-status.h>
-#include <dabbad/interface-driver.h>
-#include <dabbad/interface-pause.h>
 
-static Dabba__DabbaService_Service dabba_service =
-DABBA__DABBA_SERVICE__INIT(dabbad_);
+void dabbad_interface_pause_get(Dabba__DabbaService_Service * service,
+				const Dabba__InterfaceIdList * id_list,
+				Dabba__InterfacePauseList_Closure
+				closure, void *closure_data);
 
-int dabbad_rpc_msg_poll(void)
-{
-	ProtobufC_RPC_Server *server;
-
-	/* 0xDABA = 55994 */
-	server =
-	    protobuf_c_rpc_server_new(PROTOBUF_C_RPC_ADDRESS_TCP, "55994",
-				      (ProtobufCService *) & dabba_service,
-				      NULL);
-
-	for (;;)
-		protobuf_c_dispatch_run(protobuf_c_dispatch_default());
-
-	protobuf_c_rpc_server_destroy(server, 0);
-
-	return 0;
-}
+#endif				/* INTERFACE_PAUSE_H */
