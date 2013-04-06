0a37a583fa6d23252a5ed01357e50ced69348ab2
Emmanuel Roullit <emmanuel.roullit@gmail.com>
2013-04-06 14:02:31 +0200

    dabba: remove obsolete/unused rpc_dummy_print().
    Signed-off-by: Emmanuel Roullit <emmanuel.roullit@gmail.com>

--- dabba/include/dabba/rpc.h
+++ dabba/include/dabba/rpc.h
@@ -32,7 +32,6 @@ ProtobufCService *dabba_rpc_client_connect(const char *const name,
 void dabba_rpc_call_is_done(protobuf_c_boolean * is_done);
 void __rpc_error_code_print(const int error_code);
 void rpc_header_print(const char *const title);
-void rpc_dummy_print(const Dabba__Dummy * result, void *closure_data);
 void rpc_error_code_print(const Dabba__ErrorCode const *result,
 			  void *closure_data);
 
