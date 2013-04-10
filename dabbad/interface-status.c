3386acf7c9cc803c3735bfa2b66caaa7433918eb
Emmanuel Roullit <emmanuel.roullit@gmail.com>
2013-04-10 19:44:25 +0200

    dabbad: removed interface status unnecessary cast.
    Signed-off-by: Emmanuel Roullit <emmanuel.roullit@gmail.com>

--- dabbad/interface-status.c
+++ dabbad/interface-status.c
@@ -84,8 +84,7 @@ static void __interface_status_get(struct nl_object *obj, void *arg)
 	flags = rtnl_link_get_flags(link);
 
 	statusp->status->code =
-	    dev_link_get(statusp->id->name,
-			 (uint32_t *) & statusp->connectivity);
+	    dev_link_get(statusp->id->name, &statusp->connectivity);
 
 	statusp->loopback = (flags & IFF_LOOPBACK) == IFF_LOOPBACK;
 	statusp->up = (flags & IFF_UP) == IFF_UP;
