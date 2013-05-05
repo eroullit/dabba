949d965e8025aa6c44f30f93adc72e5c285a1b33
Emmanuel Roullit <emmanuel.roullit@gmail.com>
2013-05-05 19:46:52 +0200

    test: compare interface port to ethtool output.
    Added tests to check if the output generated by ethtool about the
    interface supports ports matches the one reported by dabba.
    
    Signed-off-by: Emmanuel Roullit <emmanuel.roullit@gmail.com>

--- dabba/test/t1003-interface-capabilities.sh
+++ dabba/test/t1003-interface-capabilities.sh
@@ -21,6 +21,25 @@ test_description='Test dabba interface capabilities command'
 
 . ./dabba-test-lib.sh
 
+ethtool_port_parse() {
+    local ethtool_output="$1"
+    local out="{"
+    local status=""
+
+    for port in aui mii bnc tp fibre
+    do
+        # append port and its related status to the output
+        grep "Supported ports:" ethtool_output | grep -qi "$port"
+        test $? = 0 && status="True" || status="False"
+        out="${out}'$port': $status, "
+    done
+
+    # replace final comma-space by a closing curly brace
+    out=$(echo "$out" | sed -e 's/, $/}/g')
+
+    echo "$out"
+}
+
 test_expect_success 'invoke dabba interface capabilities command w/o dabbad' "
     test_expect_code 22 $DABBA_PATH/dabba interface capabilities get
 "
@@ -36,6 +55,28 @@ test_expect_success 'invoke dabba interface capabilities command with dabbad' "
 test_expect_success PYTHON_YAML "Parse interface capabilities YAML output" "
     yaml2dict result > parsed
 "
+
+for i in `seq 0 $(($(number_of_interface_get)-1))`
+do
+    test_expect_success PYTHON_YAML "Query interface capabilities output" "
+        dictkeys2values interfaces $i name < parsed > output_name &&
+        dictkeys2values interfaces $i capabilities port < parsed > output_port
+    "
+
+    dev=$(cat output_name)
+
+    test_expect_success ETHTOOL,PYTHON_YAML "Query interface '$dev' capabilities via ethtool" "
+        test_might_fail '$ETHTOOL_PATH' '$dev' > ethtool_output
+    "
+
+    test_expect_success ETHTOOL,PYTHON_YAML "Parse '$dev' supported ports" "
+        ethtool_port_parse ethtool_output > ethtool_port_parsed
+    "
+
+    test_expect_success ETHTOOL,PYTHON_YAML "Check '$dev' supported ports" "
+        test_cmp ethtool_port_parsed output_port
+    "
+done
 
 test_expect_success "Cleanup: Stop dabbad" "
     killall dabbad
