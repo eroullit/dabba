ea958373eec5383f1d9341df510452f893c0c3b5
Emmanuel Roullit <emmanuel.roullit@gmail.com>
2013-06-17 22:06:55 +0200

    tx: add thread function to replay pcap file.

---
+++ libdabba/packet_tx.c
@@ -0,0 +1,102 @@
+/**
+ * \file packet_tx.c
+ * \author written by Emmanuel Roullit emmanuel.roullit@gmail.com (c) 2012
+ * \date 2012
+ */
+
+/* __LICENSE_HEADER_BEGIN__ */
+
+/*
+ * Copyright (C) 2012	Emmanuel Roullit <emmanuel.roullit@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin St, Fifth Floor, Boston, MA 02110, USA
+ *
+ */
+
+ /* __LICENSE_HEADER_END__ */
+
+#include <stdio.h>
+#include <assert.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/uio.h>
+#include <poll.h>
+
+#include <libdabba/packet_tx.h>
+#include <libdabba/pcap.h>
+
+int packet_tx_loss_set(const int sock, const int discard)
+{
+	return (setsockopt
+		(sock, SOL_PACKET, PACKET_LOSS, (void *)&discard,
+		 sizeof(discard)));
+}
+
+/**
+ * \brief Transmit packets coming from a packet mmap TX ring
+ * \param[in] arg	Pointer to packet tx thread structure
+ * \return Always return NULL
+ */
+
+void *packet_tx(void *arg)
+{
+	struct packet_tx *pkt_tx = arg;
+	struct packet_mmap *pkt_mmap = &pkt_tx->pkt_mmap;
+	struct pollfd pfd;
+	size_t index = 0;
+	ssize_t obytes;
+	int eof = 0;
+
+	if (!arg)
+		return NULL;
+
+	memset(&pfd, 0, sizeof(pfd));
+
+	pfd.events = POLLOUT;
+	pfd.fd = pkt_mmap->pf_sock;
+
+	do {
+		struct packet_mmap_header *mmap_hdr;
+
+		for (index = 0; index < pkt_mmap->layout.tp_frame_nr; index++) {
+			mmap_hdr = pkt_mmap->vec[index].iov_base;
+
+			if ((mmap_hdr->tp_h.tp_status & TP_STATUS_AVAILABLE) ==
+			    TP_STATUS_AVAILABLE) {
+				uint8_t *pkt =
+				    (uint8_t *) mmap_hdr +
+				    TPACKET_ALIGN(sizeof(mmap_hdr->tp_h));
+
+				obytes = pcap_read(pkt_tx->pcap_fd, pkt,
+						   pkt_mmap->
+						   layout.tp_frame_size);
+
+				if (obytes <= 0) {
+					eof = 1;
+					break;
+				}
+
+				mmap_hdr->tp_h.tp_len = obytes;
+				mmap_hdr->tp_h.tp_snaplen = obytes;
+				mmap_hdr->tp_h.tp_status =
+				    TP_STATUS_SEND_REQUEST;
+			}
+		}
+
+		send(pkt_mmap->pf_sock, NULL, 0, MSG_DONTWAIT);
+	} while (!eof);
+
+	return NULL;
+}
