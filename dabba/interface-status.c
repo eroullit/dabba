c18b021e060521cadb37c8fd1be1ab62ac17f440
Emmanuel Roullit <emmanuel.roullit@gmail.com>
2013-08-20 22:59:43 +0200

    dabba: switch project to MIT license.
    Signed-off-by: Emmanuel Roullit <emmanuel.roullit@gmail.com>

--- dabba/interface-status.c
+++ dabba/interface-status.c
@@ -1,31 +1,9 @@
 /**
  * \file interface-status.c
- * \author written by Emmanuel Roullit emmanuel.roullit@gmail.com (C) 2012
- * \date 2012
+ * \author written by Emmanuel Roullit emmanuel.roullit@gmail.com (C) 2013
+ * \date 2013
  */
 
-/* __LICENSE_HEADER_BEGIN__ */
-
-/*
- * Copyright (C) 2012	Emmanuel Roullit <emmanuel.roullit@gmail.com>
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or (at
- * your option) any later version.
- *
- * This program is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
- * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
- * for more details.
- *
- * You should have received a copy of the GNU General Public License along
- * with this program; if not, write to the Free Software Foundation, Inc.,
- * 51 Franklin St, Fifth Floor, Boston, MA 02110, USA
- *
- */
-
-/* __LICENSE_HEADER_END__ */
 
 /*
 
@@ -127,9 +105,9 @@ Written by Emmanuel Roullit <emmanuel.roullit@gmail.com>
 
 =over
 
-=item Copyright (C) 2012 Emmanuel Roullit.
+=item Copyright (C) 2013 Emmanuel Roullit.
 
-=item License GPLv2+: GNU GPL version 2 or later <http://gnu.org/licenses/gpl.html>.
+=item License MIT: <www.opensource.org/licenses/MIT>
 
 =item This is free software: you are free to change and redistribute it.
 
--- dabbad/interface-offload.c
+++ dabbad/interface-offload.c
@@ -1,31 +1,9 @@
 /**
  * \file interface-offload.c
- * \author written by Emmanuel Roullit emmanuel.roullit@gmail.com (c) 2012
- * \date 2012
+ * \author written by Emmanuel Roullit emmanuel.roullit@gmail.com (c) 2013
+ * \date 2013
  */
 
-/* __LICENSE_HEADER_BEGIN__ */
-
-/*
- * Copyright (C) 2012	Emmanuel Roullit <emmanuel.roullit@gmail.com>
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or (at
- * your option) any later version.
- *
- * This program is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
- * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
- * for more details.
- *
- * You should have received a copy of the GNU General Public License along
- * with this program; if not, write to the Free Software Foundation, Inc.,
- * 51 Franklin St, Fifth Floor, Boston, MA 02110, USA
- *
- */
-
-/* __LICENSE_HEADER_END__ */
 
 /* HACK prevent libnl3 include clash between <net/if.h> and <linux/if.h> */
 #ifndef _LINUX_IF_H
