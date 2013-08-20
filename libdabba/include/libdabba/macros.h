0598708d0e11cac9d5f79ac794c7cd940ab2cbc7
Emmanuel Roullit <emmanuel.roullit@gmail.com>
2013-08-20 22:00:10 +0200

    libdabba: replace container_of() macro.
    Signed-off-by: Emmanuel Roullit <emmanuel.roullit@gmail.com>

--- libdabba/include/libdabba/macros.h
+++ libdabba/include/libdabba/macros.h
@@ -44,12 +44,7 @@
  */
 
 #ifndef container_of
-#define container_of(ptr, type, member)                         \
-__extension__                                                   \
-({                                                              \
-        const typeof(((type *)0)->member) * __mptr = (ptr);	\
-        (type *)((char *)__mptr - offsetof(type, member));      \
-})
+#define container_of(ptr, type, member)	((type *)(((uint8_t *)(ptr)) - offsetof(type, member)))
 #endif				/* container_of */
 
 /**
